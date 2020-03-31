a7e4906d47cbd4aaf572b274b96c354cebf83642
turok01 <44950560+turok01@users.noreply.github.com>
2020-03-31 16:03:25 +0300

    JPA. Repair SELECT. Add @Table(name) @Column(name) for right name table and column.

--- src/main/java/com/Igor/SpringMPS/entities/TransformerSubst.java
+++ src/main/java/com/Igor/SpringMPS/entities/TransformerSubst.java
@@ -5,20 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 //@RequiredArgsConstructor
 //@NoArgsConstructor //(access = AccessLevel.PRIVATE, force = true)
 @Entity
+@Table (name = "transformersubstations")
 public class TransformerSubst {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @Column (name="NAMESUBS")
     private String nameSubst;
     private String IP;
     private String zone;
