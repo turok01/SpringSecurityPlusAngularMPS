acf5d6ec844e76dff56f593b35687ad519622cba
turok01 <44950560+turok01@users.noreply.github.com>
2020-04-07 17:28:47 +0300

    save work (adding Security)

---
+++ src/main/java/com/Igor/SpringMPS/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.Igor.SpringMPS.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new StandardPasswordEncoder("53cr3t");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                    .access("hasRole('ROLE_USER')")
+                .antMatchers("/","/**")
+                    .access("permitAll");
+    }
+
+}
