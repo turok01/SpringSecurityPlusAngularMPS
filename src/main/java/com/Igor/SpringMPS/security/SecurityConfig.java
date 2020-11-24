809539852ddb8c9533d9ed04d11670b7cdc9b332
turok01 <toolbox@tut.by>
2020-11-24 23:28:20 +0300

    replace in SecurityConfig a hasRole('ROLE_USER') with hasAnyAuthority('USER')

--- src/main/java/com/Igor/SpringMPS/security/SecurityConfig.java
+++ src/main/java/com/Igor/SpringMPS/security/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.Igor.SpringMPS.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import java.security.AuthProvider;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +20,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder encoder(){
         return new StandardPasswordEncoder("53cr3t");
     }
+
+    //@Autowired
+    //private AuthProvider authProvider;
+
+    //@Override
+    //protected void configure(AuthenticationManagerBuilder auth)
+    //{
+    //    auth.authenticationProvider(authProvider);
+    //}
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         //you have to disable csrf Protection because it is enabled by default in spring
@@ -24,11 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 //.antMatchers("/select", "/addnew","/edit/current")
                      .antMatchers("/addnew","/edit/current")
-                       .access("hasRole('ROLE_USER')" )
+                     //  .access("hasRole('ROLE_USER')" )
+                .access("hasAnyAuthority('USER')" )
 
                 //.antMatchers("/rest/**","/","/**")
-                .antMatchers("/rest/**")
+                .antMatchers("/rest/**","/login")
                     .access("permitAll")
+                .anyRequest().authenticated()
         .and()
                 .formLogin().loginPage("/login")
                 .defaultSuccessUrl("/select", true)
